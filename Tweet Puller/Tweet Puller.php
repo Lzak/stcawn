17c2a4b44212bbc2a0c63a940eca1288e7b4a903
Lasha Zakariashvili <Lasha@Lashas-MBP.home>
2015-09-13 16:05:47 -0400

    Going to start the tweet rotation.

--- Tweet Puller/Tweet Puller.php
+++ Tweet Puller/Tweet Puller.php
@@ -25,20 +25,23 @@ $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
 
 class SampleConsumer extends OauthPhirehose {
   public function enqueueStatus($status) {
-    $data = json_decode($status, true);
+  
+  	
+    /*$data = json_decode($status, true);
     if (is_array($data) && isset($data['user']['screen_name'])) {
 		$myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
 		fwrite($myfile, $data['user']['screen_name'] . ':' . urldecode($data['text']) . "\n");
 		fclose($myfile);
-    }
+    }*/
   }
 }
-// The OAuth credentials you received when registering your app at Twitter
+// OAuth from Twitter:
 define("TWITTER_CONSUMER_KEY", $keys['consumer_key']);
 define("TWITTER_CONSUMER_SECRET", $keys['consumer_secret']);
-// The OAuth data for the twitter account
 define("OAUTH_TOKEN", $keys['access_token']);
 define("OAUTH_SECRET", $keys['access_token_secret']);
+
+
 // Start streaming
 $sc = new SampleConsumer(OAUTH_TOKEN, OAUTH_SECRET, Phirehose::METHOD_SAMPLE);
 $sc->consume();
