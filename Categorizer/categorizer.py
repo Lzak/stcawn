e44a091cf546004dd6769328a5ff9d26e4844e5e
Lzak <Lzak95@ymail.com>
2015-10-17 02:37:14 -0400

    Began cleaning up the code.

--- Categorizer/categorizer.py
+++ Categorizer/categorizer.py
@@ -5,7 +5,7 @@ Description: The purpose of this script is to take a corpus (each element sepera
 	a newline (\n) and output a "category.data" file which prints the category of each
 	element seperated by a space.
 	
-Usage:	
+Usage:	categorizer.py (optional: -v for verbose_mode mode) [training_corpus_location] [test_corpus_location]
 '''
 
 import sys
@@ -16,17 +16,26 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn.datasets import load_files
 
-verbose = False
+verbose_mode = False
 
+#==================================================================
+#Load Data
+#==================================================================
 try:
+	# Check if 3 arguments were passed
 	if (len(sys.argv) == 4):
+	
+		#Check if the verbose command was passed
 		if (sys.argv[1] == "-v"):
-			verbose = True
+			verbose_mode = True
 			trainset_location = sys.argv[2]
 			dataset_location = sys.argv[3]
-
+			
+		#If not, then whatever user passed isn't valid
 		else:
 			raise ValueError("Three arguments, but the first one doesn't make sense!")
+			
+	# If 2 arguments, attempt to save file locations
 	else:		
 		trainset_location = sys.argv[1]
 		dataset_location = sys.argv[2]
@@ -39,10 +48,10 @@ except ValueError as err:
     print "[HINT] python categorizer.py -v [training_folder] [dataset_file]"
     sys.exit()
 
-
+#This needs some exception handling
 with open(dataset_location) as f:
     content = f.readlines()
-if (verbose == True): 
+if (verbose_mode == True): 
 	print "[STATUS] Read file successfully"
 	for i in content:
 		print ("[INPUT] %s" % i),
@@ -69,31 +78,27 @@ predicted = classifier.predict(content)
 #==================================================================
 #Print prediction to document
 #==================================================================
-
-#Debug crap
-#for doc, category in zip(content, predicted):
-#	print('%r => %s' % (doc, training_set.target_names[category]))
 	
 fileOut = open('test.data', 'w')
-if verbose is True:
+if verbose_mode is True:
 	print "[STATUS] Writing to file."
 #Print out possible categories on the first line
-if verbose is True:
+if verbose_mode is True:
 	print ("[OUTPUT]"),
 for c in training_set.target_names:
 	fileOut.write(c + '\t')
-	if verbose is True:
+	if verbose_mode is True:
 		print(c),
 fileOut.write('\n')
-if verbose is True:
+if verbose_mode is True:
 	print ('\n'),
 
 #Print out each category (array #) corresponding to each line in corpus
-if verbose is True:
+if verbose_mode is True:
 	print "[OUTPUT]",
 for c in predicted:
 	fileOut.write('%r ' % c)
-	if verbose is True:
+	if verbose_mode is True:
 		print('%r ' % c),
 fileOut.close()
 	
